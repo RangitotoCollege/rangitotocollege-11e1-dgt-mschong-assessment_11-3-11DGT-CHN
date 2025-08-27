f9d5451d5ab8108e1a948230ebdedd3a0c1825c8
Caio-Brito-Lima <178065@cloud.rangitoto.school.nz>
2025-08-27 13:35:54 +1200

    Update hello.py
    Fixed code for window and made it not be able to re-size

--- Python/hello.py
+++ Python/hello.py
@@ -1,9 +1,14 @@
 import tkinter
 m = tkinter.Tk()
-m.title("Hello World")
-label = tkinter.Label(m, text="Hello, World!")
-label.pack()
-m.geometry("200x100")
-m.button = tkinter.Button(m, text="Close", command=m.destroy)
-m.button.pack()
+title = "Caio's Cool Collections"
+m.title(title)
+width = 1080
+height = 720
+half_width = width / 2
+half_height = height / 2
+m.geometry("{}x{}".format(width, height))
+m.resizable(False, False) # Learned this code from stack overflow
+label = tkinter.Label(m, text=title, font=("Arial", 30), justify="left").grid(row=0, column=0, columnspan=3, pady=40, padx= half_width-200)
+
+m.button = tkinter.Button(m, text="Close", command=m.destroy).grid(row=1, column=1,)
 m.mainloop()
