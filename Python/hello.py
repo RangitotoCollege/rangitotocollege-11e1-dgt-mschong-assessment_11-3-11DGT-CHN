fa4e803f67236c1ca0b19d60056d72a17c352dd9
Caio-Brito-Lima <178065@cloud.rangitoto.school.nz>
2025-09-03 11:06:05 +1200

    Update hello.py
    Added basic menu buttons being framed in the middle of the screen, made the window re-sizable :)

--- Python/hello.py
+++ Python/hello.py
@@ -1,14 +1,33 @@
-import tkinter
-m = tkinter.Tk()
+from tkinter import *
+m = Tk()
 title = "Caio's Cool Collections"
 m.title(title)
+
+#Setting window size and boundaries
 width = 1080
 height = 720
-half_width = width / 2
-half_height = height / 2
 m.geometry("{}x{}".format(width, height))
-m.resizable(False, False) # Learned this code from stack overflow
-label = tkinter.Label(m, text=title, font=("Arial", 30), justify="left").grid(row=0, column=0, columnspan=3, pady=40, padx= half_width-200)
 
-m.button = tkinter.Button(m, text="Close", command=m.destroy).grid(row=1, column=1,)
+#Frame
+frame = Frame(m)
+frame.pack(pady=20)
+
+#Program Title
+label = Label(frame, text=title, font=("Arial", 30))
+
+help = Button(frame, text="Help")
+menu_button1 = Button(frame, text="Game 1")
+menu_button2 = Button(frame, text="Game 2")
+menu_button3 = Button(frame, text="Game 3")
+
+
+
+label.grid(row=0, column=0, columnspan=3, sticky=W+E, pady=20)
+menu_button1.grid(row=1, column=0)
+menu_button2.grid(row=1, column=1)
+menu_button3.grid(row=1, column=2)
+
+
+
+
 m.mainloop()
